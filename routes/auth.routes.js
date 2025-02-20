534cb07d6f16e418cb90542be1126a45bed393a8
jtaren <arenjatau@gmail.com>
2025-02-20 23:24:45 +0100

    auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
+import { signUp, signIn, signOut } from '../controllers/auth.controllers.js';
+
 const authRouter = Router();
 
-authRouter.post('/sign-up', (req, res) => res.send({ title: 'Sign Up' })); // Send a plain object
-authRouter.post('/sign-in', (req, res) => res.send({ title: 'Sign In' })); // Send a plain object
-authRouter.post('/sign-out', (req, res) => res.send({ title: 'Sign Out' })); // Send a plain object
+authRouter.post('/sign-up', signUp); // Send a plain object
+authRouter.post('/sign-in', signIn); // Send a plain object
+authRouter.post('/sign-out', signOut); // Send a plain object
 
 export default authRouter;

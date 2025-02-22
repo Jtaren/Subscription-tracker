e7ba859e6abe5c0ad82c2d3434c40c7f2e5240a0
jtaren <arenjatau@gmail.com>
2025-02-22 10:31:28 +0100

    app.js

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
+app.use(arcjetMiddleware)
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
@@ -22,15 +24,14 @@ app.use('/api/v1/subscriptions', subscriptionRouter);
 app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Subscription Tracker API!'); // Correct way to send a string
-});
-
-const hostname = 'localhost'; // Define hostname as a variable
-
-app.listen(PORT, hostname, async () => { // Correct way to use app.listen
-    console.log(`Subscription Tracker API is running on http://${hostname}:${PORT}`); // Template literal for cleaner output
-
+    res.send('Welcome to the Subscription Tracker API!');
+  });
+  
+  app.listen(PORT, async () => {
+    console.log(`Subscription Tracker API is running on http://localhost:${PORT}`);
+  
     await connectToDatabase();
-});
+  });
+  
 
 export default app;

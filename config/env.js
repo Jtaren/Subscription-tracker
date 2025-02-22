986352523f04e8ad3f9399d9882e240ea9e79cb6
jtaren <arenjatau@gmail.com>
2025-02-22 10:29:57 +0100

    env.js

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@ import { config } from 'dotenv';
 
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` }); // Use template literal
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, JWT_EXPIRES_IN, ARCJET_ENV, ARCJET_KEY } = process.env;
 
 
 
